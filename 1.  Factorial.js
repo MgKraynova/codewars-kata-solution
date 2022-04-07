4c5b75d9824b3d72b6df29af7b6b379f7c27ad1c
Marina Kraynova <kraynovamg@gmail.com>
2022-04-07 17:18:52 +0300

    commit

---
+++ 1.  Factorial.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/57a049e253ba33ac5e000212/train/javascript
+
+function factorial(n){
+    if (n === 0) {
+        return 1;
+    } else {
+        let result = 1;
+        for (let i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+}
