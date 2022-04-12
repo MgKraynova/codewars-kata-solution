9415715988e37d7fca631f6a4b18c789260b9f8a
Marina Kraynova <kraynovamg@gmail.com>
2022-04-12 14:12:06 +0300

    add new solutions

---
+++ 7. Sort arrays - 1.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/51f41b98e8f176e70d0002a8/train/javascript
+
+sortme = function (names) {
+    return names.sort();
+}
+
+sortme(['one', 'two', 'three']);
---
+++ 8. Multiplication table.js
@@ -0,0 +1,35 @@
+// https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/train/javascript
+
+// Your task, is to create NxN multiplication table, of size provided in parameter.
+//
+//     for example, when given size is 3:
+//
+// 1 2 3
+// 2 4 6
+// 3 6 9
+// for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
+
+multiplicationTable = function(size) {
+
+    let table = [];
+
+    for (let i = 0; i < size; i++) {
+        let firstNumberInLine = size - i;
+        //console.log('firstNumberInLine', firstNumberInLine);
+
+        let row = [];
+
+        for (let i = 0; i < size; i++) {
+            let itemInRow = firstNumberInLine + i * firstNumberInLine;
+            //console.log('itemInRow', itemInRow);
+            row.push(itemInRow);
+        }
+
+        //console.log(row);
+        table.push(row);
+    }
+    console.log(table.reverse());
+    return table.reverse();
+}
+
+multiplicationTable(3);
