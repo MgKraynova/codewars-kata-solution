dae7d0266fdd020b0a4abbe9bfa5e4146719e371
Marina Kraynova <kraynovamg@gmail.com>
2022-04-13 14:49:34 +0300

    add new solutions

---
+++ 6 kyu/1. Array Helpers.js
@@ -0,0 +1,75 @@
+// https://www.codewars.com/kata/525d50d2037b7acd6e000534/train/javascript
+
+
+Array.prototype.square = function () {
+    return this.map(function (item) {
+        return item * item;
+    });
+}
+
+Array.prototype.cube = function () {
+    return this.map(function (item) {
+        return item * item * item;
+    });
+}
+
+Array.prototype.average = function () {
+
+    if (this.length === 0) {
+        return NaN;
+    } else {
+        let sum = 0;
+
+        this.forEach((item) => {
+            sum = sum + item;
+        })
+
+        return sum / this.length;
+    }
+}
+
+Array.prototype.sum = function () {
+
+        let sum = 0;
+
+        this.forEach((item) => {
+            sum = sum + item;
+        })
+
+        return sum;
+
+}
+
+Array.prototype.even = function () {
+
+    let evenNumbers = [];
+
+    this.forEach((item) => {
+        if (item % 2 === 0) {
+            evenNumbers.push(item);
+        }
+    })
+
+    return evenNumbers;
+}
+
+Array.prototype.odd = function () {
+
+    let oddNumbers = [];
+
+    this.forEach((item) => {
+        if (!(item % 2 === 0)) {
+            oddNumbers.push(item);
+        }
+    })
+
+    return oddNumbers;
+}
+// console.log(Array.prototype);
+
+let b = [2, 5];
+b.square();
+b.cube();
+b.average();
+b.even();
+console.log(b);
---
+++ 7 kyu/2. Gauß needs help!.js
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/54df2067ecaa226eca000229/train/javascript
+
+function f(n){
+    let sum = 0;
+    if (!(typeof n === 'number')) {
+        console.log('false');
+        return false;
+    } else if (n <= 0) {
+        console.log('false');
+        return false;
+    } else if (!Number.isInteger(n)) {
+        return false;
+    } else {
+        for (let i = 0; i <= n; i++) {
+            sum = sum + i;
+        }
+        console.log(sum);
+        return sum;
+    }
+}
+
+f(100);
+f(300);
+f(5000);
+f(-10);
+f('6');
