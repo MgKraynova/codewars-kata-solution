bd4e92eb3ebf02cfe6a9730ac00c28a476c517cf
Marina Kraynova <kraynovamg@gmail.com>
2022-04-15 20:15:48 +0300

    add new solutions

---
+++ 7 kyu/5. Bumps in the Road.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/57ed30dde7728215300005fa/train/javascript
+
+function bump(x){
+    let arrayOfLetters = x.split('');
+
+    let numberOfBumps = 0;
+    
+    arrayOfLetters.forEach((letter) => {
+        if (letter === 'n') {
+            numberOfBumps = numberOfBumps + 1;
+        }
+    })
+
+    if (numberOfBumps <= 15) {
+        console.log('Woohoo!');
+        return 'Woohoo!';
+    } else {
+        console.log('Car Dead');
+        return 'Car Dead';
+    }
+}
+
+bump('_nnnnnnn_n__n______nn__nn_nnn');
---
+++ 8 kyu/2. Cat years, Dog years.js
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/train/javascript
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+    let catYears = 0;
+    let dogYears = 0;
+
+    for (let i = 1; i <= humanYears; i++) {
+        if (i === 1) {
+            catYears = 15;
+            dogYears = 15;
+        } else if (i === 2) {
+            catYears = catYears + 9;
+            dogYears = dogYears + 9;
+        } else {
+            catYears = catYears + 4;
+            dogYears = dogYears + 5;
+        }
+    }
+    return [humanYears,catYears,dogYears];
+}
+
+humanYearsCatYearsDogYears(10);
