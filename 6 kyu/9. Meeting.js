3069a774461e0e33eb4acdb9636055df994bece1
Marina Kraynova <kraynovamg@gmail.com>
2022-04-21 20:39:17 +0300

    add new solutions

---
+++ 6 kyu/9. Meeting.js
@@ -0,0 +1,30 @@
+// https://www.codewars.com/kata/59df2f8f08c6cec835000012/train/javascript
+
+function meeting(s) {
+    const arrayOfFriends = s.split(';');
+
+    //console.log(arrayOfFriends);
+
+    const arrayOfDividedFriendsFirstAndLastName = [];
+
+    arrayOfFriends.forEach((friend) => {
+        arrayOfDividedFriendsFirstAndLastName.push(friend.split(':'));
+    })
+
+
+    const arrayOfUpperCaseNames = arrayOfDividedFriendsFirstAndLastName.map((friend) => {
+        return friend.map((name) => {
+            return name.toUpperCase();
+        })
+    })
+
+    //console.log(arrayOfUpperCaseNames);
+
+    const arrayOfStringNames = arrayOfUpperCaseNames.map((friend) => {
+       return '('.concat(friend.reverse().join(', ')).concat(')');
+    })
+
+    console.log(arrayOfStringNames.sort().join(''));
+}
+
+meeting("Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill");
