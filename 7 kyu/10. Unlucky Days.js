102630f164f4bd2f3ae99c0b4a558e59ccf502f7
Marina Kraynova <kraynovamg@gmail.com>
2022-04-30 17:08:13 +0300

    add new solutions

---
+++ 7 kyu/10. Unlucky Days.js
@@ -0,0 +1,20 @@
+function unluckyDays(year) {
+
+    let count = 0;
+    for (let i = 0; i < 12; i++) {
+        let date = new Date(year, i, 13);
+
+        console.log('i', i, 'date.getDay()', date.getDay());
+
+        if (date.getDay() === 5) {
+            count++;
+        }
+    }
+
+    console.log(count);
+}
+
+// unluckyDays(2015) == 3
+// unluckyDays(1986) == 1
+
+unluckyDays(2015);
---
+++ 7 kyu/9. Currying functions multiply all elements in an array.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/586909e4c66d18dd1800009b/train/javascript
+
+function multiplyAll(array) {
+    return function (number) {
+        return array.map((item) => {
+            return item * number
+        });
+    }
+}
+
+
+//multiplyAll([1, 2, 3])(2) = [2, 4, 6];
