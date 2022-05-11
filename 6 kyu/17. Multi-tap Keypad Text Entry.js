90bf7328bade0d92e81f3b3193bedda62038ae3e
Marina Kraynova <kraynovamg@gmail.com>
2022-05-11 16:27:51 +0300

    add new solutions

---
+++ 6 kyu/17. Multi-tap Keypad Text Entry.js
@@ -0,0 +1,24 @@
+// https://www.codewars.com/kata/54a2e93b22d236498400134b/train/javascript
+
+function presses(phrase) {
+    const buttons = ['1', 'ABC2', 'DEF3', 'GHI4', 'JKL5', 'MNO6', 'PQRS7', 'TUV8', 'WXYZ9', '*', ' 0', '#'];
+
+    const lettersInPhrase = phrase.toUpperCase().split('');
+
+    let total = 0;
+
+    lettersInPhrase.forEach((letter) => {
+        buttons.forEach((button) => {
+            if (button.includes(letter)) {
+                total = total + 1 + button.indexOf(letter);
+            }
+        })
+    })
+
+    return total;
+}
+
+presses('WHERE DO U WANT 2 MEET L8R'); // 47
+presses("LOL"); // 9
+
+presses("HOW R U"); // 13
