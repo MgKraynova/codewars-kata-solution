cc1a9ec883905942460e350d735acb2e8f28df89
Marina Kraynova <kraynovamg@gmail.com>
2023-03-25 19:40:14 +0300

    add new solutions

---
+++ 5 kyu/11. Extract the domain name from a URL.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/514a024011ea4fb54200004b/train/javascript
+
+function domainName(url) {
+    const deletedHttpAndSlashes = url.replace(/https?:\/\//gi, "");
+
+    const deletedWWW = deletedHttpAndSlashes.replace(/^w{3}\.?/gi, '');
+
+    const deletedAllAfterPoint = deletedWWW.replace(/\..*/, '');
+
+    return deletedAllAfterPoint;
+}
+
+// console.log(domainName('https://www.o2b2kn.fr/default.html'))
+// console.log(domainName('http://google'));
+console.log(domainName('http://www.okv3l.tv/index.php'));
