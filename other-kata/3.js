72be305392fdfcd7f39aef5a8df9e211f57a02f8
Marina Kraynova <kraynovamg@gmail.com>
2023-11-25 20:20:52 +0300

    add new kata

---
+++ other-kata/3.js
@@ -0,0 +1,10 @@
+// Напишите функцию sum, которая работает таким образом: sum(a)(b) = a+b.
+
+function sum(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+
+console.log(sum(1)(2)); // 3
+console.log(sum(5)(-1)); // 4
