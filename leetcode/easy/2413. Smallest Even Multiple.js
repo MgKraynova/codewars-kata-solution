f5a276c0d0c2b9665da639fae17556ec4c7d8dec
mgkraynova <Marina.Kraynova@leroymerlin.ru>
2025-05-11 13:59:47 +0300

    add 2413. Smallest Even Multiple

---
+++ leetcode/easy/2413. Smallest Even Multiple.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/smallest-even-multiple/description/
+
+var smallestEvenMultiple = function (n) {
+    if (n % 2 === 0) {
+        return n
+    }
+
+    return n * 2;
+};
